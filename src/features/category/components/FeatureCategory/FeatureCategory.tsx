014fe87f14317cbeb24dd8929eac7fa1dd15ee3d
buithachit <buithach.it@gmail.com>
2025-07-24 14:42:17 +0700

    Fix build

--- src/features/category/components/FeatureCategory/FeatureCategory.tsx
+++ src/features/category/components/FeatureCategory/FeatureCategory.tsx
@@ -7,7 +7,7 @@ import { ROUTES } from '@/config/routes';
 import { filterProducts } from '@/features/product/utils/filterProduct';
 import { sortProducts } from '@/features/product/utils/sortProduct';
 import ProductList from '@/features/product/components/FeatureProduct/ProductList';
-import { SortSelect } from '@/features/product/components/FilterProduct/SortSelect.tsx';
+import { SortSelect } from '@/features/product/components/FilterProduct/SortSelect';
 import FilterToggle from '@/features/product/components/FilterProduct/FilterToggle';
 
 
