aad427530a65f29c8438f8981e63a3b50f9fff6f
buithachit <buithach.it@gmail.com>
2025-07-24 13:51:07 +0700

    Qua met moi

--- src/features/category/components/FeatureCategory/FeatureCategory.tsx
+++ src/features/category/components/FeatureCategory/FeatureCategory.tsx
@@ -1,14 +1,14 @@
-import ProductList from '@/features/product/components/FeatureProduct/ProductList';
-import FilterToggle from '@/features/product/components/FilterProduct/FilterProductComponent';
 import { parseFilter } from '@/features/product/utils/parseFilter';
 import { getProductByCategory } from '@/lib/action/product';
 import { ProductCategoryListType } from '@/types/product';
 import { DEFAULT_PAGE_SIZE } from '@/consts/pagination';
 import Pagination from '@/components/paginations/pagination';
 import { ROUTES } from '@/config/routes';
+import { SortSelect } from '@/features/product/components/FilterProduct/SortProductComponent';
 import { filterProducts } from '@/features/product/utils/filterProduct';
 import { sortProducts } from '@/features/product/utils/sortProduct';
-import { SortSelect } from '@/features/product/components/FilterProduct/SortProductComponent';
+import FilterToggle from '@/features/product/components/FilterProduct/FilterProductComponent';
+import ProductList from '@/features/product/components/FeatureProduct/ProductList';
 
 type Props = {
   params: { slug: string };
